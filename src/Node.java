c2aa443c8322929973b8e49e7f5cade5d1fb39c2
RoiPeleg <37403170+RoiPeleg@users.noreply.github.com>
2019-11-20 13:49:01 +0200

    added class structure and replaced parsers system

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Node {
+    private String Name;
+    private ArrayList<Node> parents;//stores parents
+    private ArrayList<Node> children;//stores all children
+    private CPT cpt;
+
+    private class CPT {
+    }
+}
--- src/Parser.java
+++ src/Parser.java
@@ -1,37 +1,45 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.RegularExpression;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-
+import java.util.regex.Matcher;
 /**
  * Parses input txt to create it's matching graph
  */
 public class Parser {
     /**
-     * gets the input file and give constructors the matching values
+     * gets the input file and and sorts it
      * @param input input file path
      * @throws Exception
      */
-    public static void getInput(String input) throws Exception {
+    public static StringBuilder getInput(String input) throws Exception {
         FileReader InputFile = new FileReader(input);
         BufferedReader reader = new BufferedReader(InputFile);
+        StringBuilder contents = new StringBuilder();
         String line;
-        ArrayList<String> content = new ArrayList<String>();//all lines without blanks
-
-        while ((line = reader.readLine()) != null) {
-            //if the line we're on contains the text we don't want to add, skip it
+        while(reader.ready()) {
+            line = reader.readLine();
             if (line.length() ==0) {
                 continue;
             }
-            //if we get here, we assume that we want the text, so add it
-            content.add(line);
+            contents.append(line+"\n");
         }
+        reader.close();
+        return contents;
+    }
+    public static void toConstruct()
+    {
+        try{
+            StringBuilder sb = getInput("C:\\Users\\user\\Documents\\GitHub\\AI_algo\\src\\input.txt");
+            String [] s =sb.toString().split("Var");
+                System.out.println(s[5]);
+        }catch (Exception e){System.out.println(e.getMessage());}
+
 
     }
-
     public static void main(String[] args) {
-        try{
-            getInput("C:\\Users\\user\\Documents\\GitHub\\AI_algo\\src\\input.txt");
-        }catch (Exception e){System.out.println(e.getMessage());}
+        toConstruct();
     }
 }
