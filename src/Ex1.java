171ac4e5344747c60bf6ad8f1aaba7e7f0fc6eff
RoiPeleg <37403170+RoiPeleg@users.noreply.github.com>
2019-12-17 20:25:12 +0200

    stored Queries in NodeCollection

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,6 +1,11 @@
 public class Ex1 {
     public static void main(String[] args) {
         NodeCollection nc = Parser.toConstruct();
+        String[] q = nc.getQueries();
+        for (int i=0;i<q.length;i++)
+        {
+            System.out.println("q"+i + ") "+q[i]);
+        }
         //Queries handling
         //write to output
     }
